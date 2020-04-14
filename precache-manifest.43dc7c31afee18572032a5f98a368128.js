a72fe3b5c4294dd79bca8e456d70af39df39cbf2
Sushen Birari <sushen.birari@team.telstra.com>
2020-04-14 23:03:17 +0530

    Updates

---
+++ precache-manifest.43dc7c31afee18572032a5f98a368128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45e6b82ebc33c770320a7542ccd9bec",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63d0412a9501af9c56f7",
+    "url": "/monsters-rolodex/static/css/main.f64732a4.chunk.css"
+  },
+  {
+    "revision": "41e8049d7404ff2aeafd",
+    "url": "/monsters-rolodex/static/js/2.d31c0c42.chunk.js"
+  },
+  {
+    "revision": "63d0412a9501af9c56f7",
+    "url": "/monsters-rolodex/static/js/main.bd8e7929.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
